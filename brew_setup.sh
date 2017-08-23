584bdecefe1a3d8f607a1b0f26a69cdf49201f77
Vasu Mahesh <vasu.mahesh@hotmail.com>
2017-08-23 13:02:35 -0400

    add: brew script

---
+++ brew_setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew update
+
+brew install elixir
+brew install lua
+brew install grep
+brew install wget --with-iri
+brew install the_silver_searcher
+brew install tmux
+brew install cmake
+brew install astyle
+
